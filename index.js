2361ef72058e9a355be3f3eda1e9274fe055f471
Emily Asch <e.asch@me.com>
2018-07-23 16:44:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ function myAnimal() {
   return animal
 }
 
-var  = 'cat'
 function yourAnimal() = {
+  if (var animal === 'dog'){
+    return 'cat'
+  } else return {'dog'}
+  }
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
